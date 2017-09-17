a378d19c942356479f466a6bea02c41eeda33c5e
michaelm1453 <31252185+michaelm1453@users.noreply.github.com>
2017-09-17 16:51:32 -0500

    Update HTMLChecker.java

--- E15_20/student/HTMLChecker.java
+++ E15_20/student/HTMLChecker.java
@@ -2,6 +2,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 /**
    Write a program that checks whether a sequence of HTML tags
    is properly nested. For each opening tag, such as <p>, there
@@ -17,41 +19,28 @@ public class HTMLChecker
 {
    public static void main(String[] args)
    {
-      String filename = "TagSample1.html";
+      String filename = "TagSample3.html";
       Stack<String> stacked = new Stack<>();
-	  String next = "";
+      String next = "";
       try (Scanner in = new Scanner(new File(filename)))
       {
          // Your code goes here
          while(in.hasNext()){//This loop goes through and takes out the opening tags (the ones without '/').
-         //So, we need to check if the first one removed from the stack (last one added) is the first one encountered by the thing
-			 next = in.next();
-			 System.out.println(next);
-			 if(!(next.contains("/")))
-			 	stacked.push(next);
-			 else{
-			 	if(next == stacked.pop())
-
-			}
-
-
-			 for(String tag : stacked){
-				String sub = tag.substring(tag.indexOf('<')+1, tag.indexOf('>'));
-				if(next.contains(sub))
-					System.out.println(sub + next);
-					//last one into stack, first one to be taken out
-					//last tag that was put in, needs to be the first one encountered when tested
-
-			 }
-
-		 }
-		 System.out.println(stacked);
-
-
-
-
-
-
+             next = in.next();
+             if(!(next.contains("/")))
+                stacked.push(next);
+             else{
+                String fromStack = stacked.peek();
+                String subStack = fromStack.substring(fromStack.indexOf('<')+1, fromStack.indexOf('>'));
+                if(next.contains(subStack))
+                    stacked.pop();
+                }
+            }    
+        
+         if(stacked.isEmpty())
+            System.out.println("This is in HTML style");
+         else
+            System.out.println("This is not in HTML style");
       }
       catch (FileNotFoundException e)
       {
