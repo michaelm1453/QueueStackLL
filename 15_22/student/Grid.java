c9ef98df1622205b095a71653ce631b380816776
michaelm1453 <31252185+michaelm1453@users.noreply.github.com>
2017-09-21 12:20:02 -0500

    9/21

--- 15_22/student/Grid.java
+++ 15_22/student/Grid.java
@@ -5,31 +5,33 @@ public class Grid
 {
    private static final int SIZE = 10;
    int[][] pixels = new int[SIZE][SIZE];
-   Queue<Integer> numbers = new LinkedList<>();
-
+   int num = 1;
+	//So you need to make a pair class to hold the x and y values for the coordinates
+	//You need to use all if statements, otherwise it'll go on the first one only
+	//Call the floodfill recursively to go to the next coordinate
+	//Needs a starting number (1), and then after you fill a spot, increase the number
 
    /**
       Flood fill, starting with the given row and column.
    */
    public void floodfill(int row, int column)
    {
-	  for(int x = 1; x <= SIZE*SIZE; x ++){
-	  	  numbers.add(x);
-   	  }
-      while(numbers.size() > 0){
-	      if(pixels[row][column] == null){
-		    pixels[row][column] = numbers.remove();	
-	      }
-	      else{
-		    if(pixels[row+1][column] ==null)
-			    row++
-	            else if(pxels[row][column+1] ==null)
-			    column++;
-		    else if(pixels[row-1][column] ==null)
-			    row --;
-		    else if(pixells[row][column-1] == null)
-			    column--;
-	      }
+      while(num <101){
+	      if(pixels[row][column] == 0){
+		    pixels[row][column] = num;
+		    num ++;
+  			}
+		    if(row+1 < SIZE && pixels[row+1][column] ==0){
+			    	num++;
+			}
+
+	        if(column +1 < SIZE && pixels[row][column+1] == 0)
+			    num++;
+		    if(row -1 >=0 &&pixels[row-1][column] ==0)
+			    num ++;
+		    if(column-1 >= 0&&pixels[row][column-1] ==0)
+			    num++;
+
 	  }
    }
 
