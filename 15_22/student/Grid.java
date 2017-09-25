05f1fdce743ba92edd4f194bc6205ea056c94c58
michaelm1453 <31252185+michaelm1453@users.noreply.github.com>
2017-09-24 21:10:53 -0500

    Update Grid.java

--- 15_22/student/Grid.java
+++ 15_22/student/Grid.java
@@ -6,6 +6,8 @@ public class Grid
    private static final int SIZE = 10;
    int[][] pixels = new int[SIZE][SIZE];
    int num = 1;
+   int x = 0; int y = 0; int number = 0;
+   Queue<Coordinate> coords = new LinkedList<>();
 	//So you need to make a pair class to hold the x and y values for the coordinates
 	//You need to use all if statements, otherwise it'll go on the first one only
 	//Call the floodfill recursively to go to the next coordinate
@@ -16,21 +18,42 @@ public class Grid
    */
    public void floodfill(int row, int column)
    {
-      while(num <101){
-	      if(pixels[row][column] == 0){
-		    pixels[row][column] = num;
-		    num ++;
+      while(num <100){
+	        if(pixels[row][column] == 0){//for the first point
+	    	    coords.add(new Coordinate(row, column, num));  //adds to the queue a coordinate with a value
+		        num ++;
+		        System.out.println(row+ " " + column+ " " + num);
   			}
-		    if(row+1 < SIZE && pixels[row+1][column] ==0){
-			    	num++;
+		    if(row+1 < 10&& pixels[row+1][column] ==0){//checks south
+		    	coords.add(new Coordinate(row+1, column, num));  //adds to the queue a coordinate with a value
+			    num++;
+			    System.out.println((row+1) + " " + column +" " + num);
 			}
 
-	        if(column +1 < SIZE && pixels[row][column+1] == 0)
-			    num++;
-		    if(row -1 >=0 &&pixels[row-1][column] ==0)
-			    num ++;
-		    if(column-1 >= 0&&pixels[row][column-1] ==0)
-			    num++;
+	        if(column +1 < 10 && pixels[row][column+1] == 0){//checks east
+	        	coords.add(new Coordinate(row, column+1, num));  //adds to the queue a coordinate with a value
+	        	num++;
+	        	System.out.println(row + " " + (column+1) +" " +num);
+	        }
+			
+		    if(row -1 >=0 &&pixels[row-1][column] ==0){//checks west
+		    	coords.add(new Coordinate(row-1, column, num));  //adds to the queue a coordinate with a value
+		    	num++;
+		    	System.out.println((row-1) + " " + column+" " + num);
+		    }
+			    
+		    if(column-1 >= 0&&pixels[row][column-1] ==0){//checks south
+		    	coords.add(new Coordinate(row, column-1, num));//adds to the queue a coordinate with a value
+		    	num++;
+		    	System.out.println(row+ " " + (column-1) +" " + num);
+		    }
+			while(coords.size() > 0){
+				Coordinate c = coords.remove();
+				x = c.x; y = c.y; number = c.num;
+				pixels[x][y] = number;
+				floodfill(x,y);//goes back through the queue
+				
+			}
 
 	  }
    }
