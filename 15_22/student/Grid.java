0a0d75e181cf4ba236933eeb9a845c166e229a62
michaelm1453 <31252185+michaelm1453@users.noreply.github.com>
2017-09-26 20:25:06 -0500

    Update Grid.java

--- 15_22/student/Grid.java
+++ 15_22/student/Grid.java
@@ -1,5 +1,6 @@
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class Grid
 {
@@ -7,7 +8,7 @@ public class Grid
    int[][] pixels = new int[SIZE][SIZE];
    int num = 1;
    int x = 0; int y = 0; int number = 0;
-   Queue<Coordinate> coords = new LinkedList<>();
+   Stack<Coordinate> coords = new Stack<>();
 	//So you need to make a pair class to hold the x and y values for the coordinates
 	//You need to use all if statements, otherwise it'll go on the first one only
 	//Call the floodfill recursively to go to the next coordinate
@@ -20,41 +21,41 @@ public class Grid
    {
       while(num <100){
 	        if(pixels[row][column] == 0){//for the first point
-	    	    coords.add(new Coordinate(row, column, num));  //adds to the queue a coordinate with a value
+	    	    coords.push(new Coordinate(row, column, num));  //adds to the stack a coordinate with a value
 		        num ++;
 		        System.out.println(row+ " " + column+ " " + num);
   			}
 		    if(row+1 < 10&& pixels[row+1][column] ==0){//checks south
-		    	coords.add(new Coordinate(row+1, column, num));  //adds to the queue a coordinate with a value
+		    	coords.push(new Coordinate(row+1, column, num));  //adds to the stack  a coordinate with a value
 			    num++;
 			    System.out.println((row+1) + " " + column +" " + num);
 			}
 
 	        if(column +1 < 10 && pixels[row][column+1] == 0){//checks east
-	        	coords.add(new Coordinate(row, column+1, num));  //adds to the queue a coordinate with a value
+	        	coords.push(new Coordinate(row, column+1, num));  //adds to the stack a coordinate with a value
 	        	num++;
 	        	System.out.println(row + " " + (column+1) +" " +num);
 	        }
 
 		    if(row -1 >=0 &&pixels[row-1][column] ==0){//checks west
-		    	coords.add(new Coordinate(row-1, column, num));  //adds to the queue a coordinate with a value
+		    	coords.push(new Coordinate(row-1, column, num));  //adds to the stack a coordinate with a value
 		    	num++;
 		    	System.out.println((row-1) + " " + column+" " + num);
 		    }
 
 		    if(column-1 >= 0&&pixels[row][column-1] ==0){//checks south
-		    	coords.add(new Coordinate(row, column-1, num));//adds to the queue a coordinate with a value
+		    	coords.push(new Coordinate(row, column-1, num));//adds to the stack a coordinate with a value
 		    	num++;
 		    	System.out.println(row+ " " + (column-1) +" " + num);
 		    }
 			while(coords.size() > 0){
-				for(Coordinate c : coords)
-					System.out.println(c);
+				//for(Coordinate c : coords)
+					//System.out.println(c);
 
-				//Coordinate c = coords.remove();
-				//x = c.x; y = c.y; number = c.num;
-				//pixels[x][y] = number;
-				//floodfill(x,y);//goes back through the queue
+				Coordinate c = coords.pop();
+				x = c.x; y = c.y; number = c.num;
+				pixels[x][y] = number;
+				floodfill(x,y);//goes back through the stack
 
 			}
 
