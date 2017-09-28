91ca6e47514f463c4409af4c5df61cb4ba8800d9
michaelm1453 <31252185+michaelm1453@users.noreply.github.com>
2017-09-27 21:36:14 -0500

    Update Grid.java

--- 15_22/student/Grid.java
+++ 15_22/student/Grid.java
@@ -9,7 +9,6 @@ public class Grid
    int num = 1;
    int x = 0; int y = 0; int number = 0;
    Stack<Coordinate> coords = new Stack<>();
-   int countS = 0; int countN = 0; int countE = 0; int countW = 0;
 	//So you need to make a pair class to hold the x and y values for the coordinates
 	//You need to use all if statements, otherwise it'll go on the first one only
 	//Call the floodfill recursively to go to the next coordinate
@@ -20,41 +19,41 @@ public class Grid
    */
    public void floodfill(int row, int column)
    {
-      while(num <=100){
+      while(num <101){
 	        if(pixels[row][column] == 0){//for the first point
 	    	    coords.push(new Coordinate(row, column, num));  //adds to the stack a coordinate with a value
-		        num ++;
+		        
 		        pixels[row][column] = num; //Since the point had already been 'filled' it won't be refilled over
+		        num ++;
   			}
 	        
 	        while(coords.size() > 0){
 
 			    if(row+1 < 10 && pixels[row+1][column] ==0){//checks south
 			    	coords.push(new Coordinate(row+1, column, num));  //adds to the stack  a coordinate with a value
-				    num++; countS++;
 				    pixels[row+1][column] = num; //Since the point had already been 'filled' it won't be refilled over
+				    num ++;
 				}
 	
 		        if(column +1 < 10 && pixels[row][column+1] == 0){//checks east
 		        	coords.push(new Coordinate(row, column+1, num));  //adds to the stack a coordinate with a value
-		        	num++; countE++;
 		        	pixels[row][column+1] = num; //Since the point had already been 'filled' it won't be refilled over
+		        	num ++;
 		        }
 	
 			    if(row -1 >=0 &&pixels[row-1][column] ==0){//checks north
 			    	coords.push(new Coordinate(row-1, column, num));  //adds to the stack a coordinate with a value
-			    	num++; countN++;
 			    	pixels[row-1][column] = num; //Since the point had already been 'filled' it won't be refilled over
+			    	num ++;
 			    }
 	
 			    if(column-1 >= 0&&pixels[row][column-1] ==0){//checks west
 			    	coords.push(new Coordinate(row, column-1, num));//adds to the stack a coordinate with a value
-			    	num++; countW++;
 			    	pixels[row][column-1] = num; //Since the point had already been 'filled' it won't be refilled over
+			    	num ++;
 			    }
 			    Coordinate c = coords.pop();
-			    x = c.x; y = c.y; number = c.num;
-			    pixels[x][y] = number;
+			    x = c.x; y = c.y;
 			    floodfill(x,y);
 	        }
 
@@ -71,7 +70,18 @@ public class Grid
             r = r + String.format("%4d", pixels[i][j]);
          r = r + "\n";
       }
-      System.out.println("North" + countN + "S" + countS + "E" + countE + "W");
       return r;
    }
 }
+/**
+  21  22  23  25  27  29  31  33  35  37
+  19  20  24  26  28  30  32  34  36  38
+  17  18  51  50  49  47  45  43  41  39
+  16  13  10   7   4  48  46  44  42  40
+  14  11   8   5   1   3  52  54  56  58
+  15  12   9   6   2  70  53  55  57  59
+  79  77  75  73  71  68  66  64  62  60
+  80  78  76  74  72  69  67  65  63  61
+  81  83  85  87  89  91  93  95  97  99
+  82  84  86  88  90  92  94  96  98 100
+*/
